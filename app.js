4aa30f500ca8a18cdf793372a5a926881cbdc707
Taif Rahaman <taif.rahaman73@gmail.com>
2021-11-07 09:17:30 +0600

    basic server setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app = express()
+
+//setting up config.env file setup 
+dotenv.config({ path: './config/config.env' })
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
