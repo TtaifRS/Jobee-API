8e1f30b717ba953ea24bca664af48aeb612502a6
Taif Rahaman <taif.rahaman73@gmail.com>
2021-11-08 23:05:56 +0600

    password hasing

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -31,6 +33,10 @@ const userSchema = new mongoose.Schema({
     },
     resetPasswordToken: String,
     reseetPasswordExpire: Date
+})
+
+userSchema.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 10)
 })
 
 module.exports = mongoose.model('User', userSchema)
