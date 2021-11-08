ad1a4130535bf339f5a7d921f9823f1c12daea51
Taif Rahaman <taif.rahaman73@gmail.com>
2021-11-09 01:30:52 +0600

    login functionallity

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/users')
 const catchAsyncError = require('../middlewares/catchAsyncError')
+const ErrorHandler = require('../utils/errorHandler')
 
 //register user => /api/v1/register 
 exports.register = catchAsyncError(async (req, res, next) => {
@@ -19,4 +20,33 @@ exports.register = catchAsyncError(async (req, res, next) => {
         message: "User is registered",
         token
     })
+})
+
+//login user => /api/v1/login 
+exports.login = catchAsyncError(async (req, res, next) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return next(new ErrorHandler('Please enter Email and Password'), 400)
+    }
+
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user) {
+        return next(new ErrorHandler('Invalid email or password', 401))
+    }
+
+    const isPasswordMatched = await user.comparePassword(password)
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Invalid email or password', 401))
+    }
+
+    const token = user.getJwtToken()
+
+    res.status(200).json({
+        success: true,
+        token
+    })
+
 })
